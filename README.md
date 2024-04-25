e789096ac6eee215e1f76b58f104165a883aae80
Andrew Britz <2305109@students.wits.ac.za>
2024-04-25 12:49:27 +0200

    Added Logout Feature

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Andrew Britz <br/>
 Nduvho Ramashia <br/>
 Mfaniseni Thusi
 
-Link to Website
+Link to Website: 2024-group-lab-002.azurewebsites.net
 Project Description:
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
 
@@ -7,14 +8,31 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Session middleware
+app.use(session({
+    secret: 'secret-key', // Change this to a random string
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Middleware to check if the user is logged in
+const requireLogin = (req, res, next) => {
+    if (!req.session.displayName) {
+        return res.redirect('/');
+    }
+    next();
+};
+
 // Routes
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/loginPage.html');
 });
 
 app.post('/login', (req, res) => {
-    const password = req.body.password;
+    const { displayName, password } = req.body;
     if (password === 'brokenTelephone') {
+        // Here you can handle the display name as needed, for example, you can store it in a session
+        req.session.displayName = displayName;
         // Redirect to the main game page
         res.redirect('/game');
     } else {
@@ -22,8 +40,15 @@ app.post('/login', (req, res) => {
     }
 });
 
-app.get('/game', (req, res) => {
-    res.send('Welcome to the game page!');
+// Route to serve the game page with authentication middleware
+app.get('/game', requireLogin, (req, res) => {
+    res.sendFile(__dirname + '/public/game.html');
+});
+
+// Route to log out the user
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
 });
 
 // Server
